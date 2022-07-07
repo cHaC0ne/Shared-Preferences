90cec9bb78ce600a842173b1709bfdc80e7ff261
Alumno <asd@asd.com>
2022-07-07 17:15:42 +0200

    Prueba

--- app/src/main/java/com/example/sharedpreferences/SecondActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/SecondActivity.kt
@@ -1,7 +1,10 @@
 package com.example.sharedpreferences
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.sharedpreferences.SharedPreferences.Companion.prefs
 import com.example.sharedpreferences.databinding.ActivityMainBinding
 import com.example.sharedpreferences.databinding.ActivitySecondBinding
 
@@ -12,6 +15,20 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(b.root)
+        initUi()
 
+    }
+    fun initUi(){
+        b.bClose.setOnClickListener {
+            prefs.wipeData()
+            onBackPressed()
+        }
+        val userName = prefs.getName()
+        b.tvHello.text = "Hola $userName !"
+        if(prefs.getColorCheck()){
+            b.constLayout.setBackgroundColor(
+                ContextCompat.getColor(this, R.color.naranja)
+            )
+        }
     }
 }
